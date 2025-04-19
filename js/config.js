585482f8b3864ea5373f0dea9940bb332a7ff535
GrupoGps-Mecanizada <gpssa.mecanizada@gmail.com>
2025-04-18 23:28:06 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   VERSAO_APP: '3.0',
   
   // URL da API do Google Apps Script (importante: substitua pela URL que você copiou)
-  API_URL: 'https://script.google.com/macros/s/AKfycbwNaCFBBM1urRUmYll1L1Hxs2_294nlnSjr6GD1yJ35ao9K890qSV7nSGg2kCFgqvsh/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw4LsriwJzmSwhljn9FSxLGvRIYo5FnOJIpJImlhLYbIg-MuVI2QQN4F6vdQ2BlEMgM/exec',
   
   // ID da planilha Google Sheets 
   PLANILHA_ID: '1AyDfxSAFCmAqw512xSMCwXrIMj2JDryhjoySFGmkLvI',
