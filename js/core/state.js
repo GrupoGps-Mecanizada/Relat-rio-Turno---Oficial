ce5ee5a67ed2cdb7638b14d439f8f50d5724a048
GrupoGps-Mecanizada <gpssa.mecanizada@gmail.com>
2025-04-19 08:10:23 -0300

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -2,8 +2,10 @@
  * Sistema simples de gerenciamento de estado
  * Permite centralizar dados e notificar componentes sobre mudanças
  */
-const AppState = {
-  data: {
+
+// Registrar o módulo 'state' com o ModuleLoader
+ModuleLoader.register('state', function() {
+  const stateData = {
     currentUser: null,
     isAuthenticated: false,
     currentStep: 'stepTurno',
@@ -12,53 +14,54 @@ const AppState = {
     ultimoRelatorioId: null,
     darkMode: localStorage.getItem('darkMode') === 'true',
     cachedData: {}
-  },
+  };
   
-  listeners: {},
+  const listeners = {};
   
   // Sistema de observadores simples
-  subscribe(key, callback) {
-    if (!this.listeners[key]) this.listeners[key] = [];
-    this.listeners[key].push(callback);
-    return () => this.unsubscribe(key, callback);
-  },
+  function subscribe(key, callback) {
+    if (!listeners[key]) listeners[key] = [];
+    listeners[key].push(callback);
+    return () => unsubscribe(key, callback);
+  }
   
-  unsubscribe(key, callback) {
-    if (!this.listeners[key]) return;
-    this.listeners[key] = this.listeners[key].filter(cb => cb !== callback);
-  },
+  function unsubscribe(key, callback) {
+    if (!listeners[key]) return;
+    listeners[key] = listeners[key].filter(cb => cb !== callback);
+  }
   
   // Atualizar estado e notificar observadores
-  update(key, value) {
-    this.data[key] = value;
-    if (this.listeners[key]) {
-      this.listeners[key].forEach(callback => callback(value));
+  function update(key, value) {
+    stateData[key] = value;
+    console.log(`Estado '${key}' atualizado:`, value);
+    if (listeners[key]) {
+      listeners[key].forEach(callback => callback(value));
     }
     // Persistir alguns estados específicos
     if (key === 'darkMode') {
       localStorage.setItem('darkMode', value);
     }
-  },
+  }
   
-  get(key) {
-    return this.data[key];
-  },
+  function get(key) {
+    return stateData[key];
+  }
   
   // Método para salvar cache
-  cacheData(key, data, expirationMinutes = 30) {
+  function cacheData(key, data, expirationMinutes = 30) {
     const cacheItem = {
       data: data,
       expiry: new Date().getTime() + (expirationMinutes * 60 * 1000)
     };
     
-    const cache = this.data.cachedData || {};
+    const cache = stateData.cachedData || {};
     cache[key] = cacheItem;
-    this.update('cachedData', cache);
-  },
+    update('cachedData', cache);
+  }
   
   // Método para recuperar do cache
-  getCachedData(key) {
-    const cache = this.data.cachedData || {};
+  function getCachedData(key) {
+    const cache = stateData.cachedData || {};
     const cacheItem = cache[key];
     
     if (!cacheItem) return null;
@@ -66,13 +69,40 @@ const AppState = {
     // Verificar se expirou
     if (new Date().getTime() > cacheItem.expiry) {
       delete cache[key];
-      this.update('cachedData', cache);
+      update('cachedData', cache);
       return null;
     }
     
     return cacheItem.data;
   }
-};
-
-// Exportar o módulo para uso global
-window.AppState = AppState;
+  
+  // Função de inicialização
+  function init() {
+    console.log('Módulo State inicializado com sucesso.');
+    
+    // Expor AppState globalmente para compatibilidade
+    window.AppState = {
+      data: stateData,
+      listeners: listeners,
+      subscribe: subscribe,
+      unsubscribe: unsubscribe,
+      update: update,
+      get: get,
+      cacheData: cacheData,
+      getCachedData: getCachedData
+    };
+    
+    return window.AppState;
+  }
+  
+  // Retornar interface do módulo
+  return {
+    init: init,
+    subscribe: subscribe,
+    unsubscribe: unsubscribe,
+    update: update,
+    get: get,
+    cacheData: cacheData,
+    getCachedData: getCachedData
+  };
+});
