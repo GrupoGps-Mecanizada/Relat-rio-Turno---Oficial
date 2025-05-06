ffc391adebb8ee2ddc63dee3fffb538af426d271
GrupoGps-Mecanizada <gpssa.mecanizada@gmail.com>
2025-05-06 16:43:57 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
   
   // Definir propriedades individualmente para melhor controle
   window.CONFIG.VERSAO_APP = '3.2'; // Alterado de 3.0 para 3.1
-  window.CONFIG.API_URL = 'https://script.google.com/macros/s/AKfycbxEerPxtCZrueQ2iTpTeY8DeEsjkHC5MaU32hTWvoN21BTc5gy8AutaWuZNii0fbh9v3A/exec';
+  window.CONFIG.API_URL = 'https://script.google.com/macros/s/AKfycbxog1v8PY7TSjVpK_C3sCrDABcXH0uEmb1o0hlq6FERmbzZo2yS3YjLVxwHVoUiCH64yg/exec';
   window.CONFIG.PLANILHA_ID = '1BXQDku3C3uuOyucuAmQoV36iQMtC0bsa--4VDZQBO54';
   window.CONFIG.API_KEY = 'AIzaSyCi8G96eC3Ok9PLimQJ0OqVEe3-rJgWGaM';
   window.CONFIG.AUTH_REQUIRED = false;
